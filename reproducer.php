30b7222d8205275696b21a9c4bfa7611c7d776b3
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-10-06 19:11:22 +0200

    Reduce reproducer

--- reproducer.php
+++ reproducer.php
@@ -11,24 +11,13 @@ $messageEvents = new MessageEvents();
 $messageEvents->add(new MessageEvent($message1 = (new Email())->to('alice@example.com'), Envelope::create($message1), 'null://null'));
 $messageEvents->add(new MessageEvent($message2 = (new Email())->to('bob@example.com'), Envelope::create($message2), 'null://null'));
 
-$cache = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('message_events');
-// uncomment the next line to "make it works"
-// $cache = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('message_events', 0, null, new \Symfony\Component\Cache\Marshaller\DefaultMarshaller(false));
+var_dump($messageEvents); // Comment/uncomment to trigger the bug
 
-$item = $cache->getItem($key = 'my_key');
-$item->set($messageEvents);
+var_dump('headers_before', $messageEvents->getEvents()[0]->getMessage()->getHeaders() === $messageEvents->getEvents()[1]->getMessage()->getHeaders());
 
-var_dump($item); // Comment/uncomment to trigger the bug
+$ser = igbinary_serialize($messageEvents);
 
-$cache->save($item);
+$messageEvents = igbinary_unserialize($ser);
 
-$cache = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('message_events');
-// uncomment the next line to "make it works"
-// $cache = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('message_events', 0, null, new \Symfony\Component\Cache\Marshaller\DefaultMarshaller(false));
-
-$item = $cache->getItem($key);
-
-// should dump "false"
-var_dump('messages', $item->get()->getEvents()[0]->getMessage() === $item->get()->getEvents()[1]->getMessage());
-// should dump "false", but dump "true" the "var_dump($item)" is not commented
-var_dump('headers', $item->get()->getEvents()[0]->getMessage()->getHeaders() === $item->get()->getEvents()[1]->getMessage()->getHeaders());
+// should dump "false", but dumps "true" the "var_dump($messageEvents)" is not commented
+var_dump('headers_after', $messageEvents->getEvents()[0]->getMessage()->getHeaders() === $messageEvents->getEvents()[1]->getMessage()->getHeaders());
